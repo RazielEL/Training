41dc8c5e9b0939433d967020232b46294bd5fcc8
RazielEL <amp91@pm.me>
2022-07-04 10:08:10 -0500

    3rd commit

--- src/test/java/Utils/CommonMethods.java
+++ src/test/java/Utils/CommonMethods.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,8 +22,20 @@ public class CommonMethods extends PageInitializers {
 
     public static void openBrowserAndLanuchApplication() {
         ConfigReader.readProperties(Constants.CONFIGURATION_FILEPATH);
+
+
+//        WebDriverManager.chromedriver().setup();
+//        drv = new ChromeDriver();
+
+        // to do gory skomentowane bo Jenkins nie ogarnia
+        // to nizej musi byc zeby jenkins ogarnial
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setHeadless(true);
         WebDriverManager.chromedriver().setup();
-        drv = new ChromeDriver();
+        drv = new ChromeDriver(chromeOptions);
+
+
         drv.get(ConfigReader.getPropertyValue("url3"));
         drv.manage().window().maximize();
         drv.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
